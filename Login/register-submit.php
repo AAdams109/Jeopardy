7d4948ee846ef395c863597746969bd0eebabdb0
AAdams109 <146459915+AAdams109@users.noreply.github.com>
2023-11-14 17:52:27 -0500

    Update register-submit.php
    Bug corrected, tested properly

--- Login/register-submit.php
+++ Login/register-submit.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Registration Submission</title>
+		<title>Registration</title>
 		<link href="./.css" type="text/css" rel="stylesheet" />
 	</head>
 <?php
 /*Check user input submitted w register.php*/ 
-		if(isset($_POST["username"]) &&!empty($_POST["username"])) {
-			$username=$_POST["username"];
-		} else {
-			echo "Error, please make sure to fill in name.";
-			echo "Please <a href="register.php"> resubmit.</a>";
-		}
-		if(isset($_POST["passcode"]) &&!empty($_POST["passcode"])) {
-			$passcode=$_POST["passcode"];
-		} else {
-			echo "Error, please make sure to fill in password.";
-   			echo "Please <a href="register.php"> resubmit.</a>";
-		}
-		
+	if(isset($_POST["username"]) &&!empty($_POST["username"])) 
+	{
+		$username=$_POST["username"];
+	} else {
+		echo "Error, please make sure to fill in name.";
+	}
+	if(isset($_POST["passcode"]) &&!empty($_POST["passcode"])) 
+	{
+		$passcode=$_POST["passcode"];
+	} else {
+		echo "Error, please make sure to fill in password.";
+	}
+	
 	$checkfile = file_get_contents('./userdata.txt');	
 	$users = explode("\n",$checkfile);
 	
 	foreach($users as $checkUser)
 	{
-		$checkUser = explode(",",$users);
-		if($checkUser[0] !== $username) 
+		$checkUser = explode(",",$checkUser);
+		if($checkUser[0] != $username) 
 		{
 			$userinput = $username.",".$passcode.",0\n";
 			file_put_contents('./userdata.txt', $userinput, FILE_APPEND);
@@ -36,7 +36,7 @@
 	}
 ?>
 	<body>
-			<h1> Welcome, <?php print $username ?> "</h1>;
+			<h1> Welcome, <?php print $username ?> </h1>;
 			<p> You have been successfully registered! To play jeopardy, please <a href="login.html"> login. </a></p>
 			<p> If you need a reminder, your password is <?php print $passcode?>, make sure you do not forget!</p>
 	</body>
